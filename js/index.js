35e82836bacae0de21ba7e73fce4453bc53a838f
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-02-17 01:47:25 +0600

    get saving added

--- js/index.js
+++ js/index.js
@@ -10,6 +10,21 @@ function getTotalExpenses(food, rent, clothes) {
     const totalExpensesValue = parseFloat(totalExpenses.innerText);
     const total = food + rent + clothes + totalExpensesValue;
     totalExpenses.innerText = total;
+    return total;
+}
+function getBalance(expenses, salary) {
+    const balance = document.getElementById('balance');
+    const previousBalance = parseFloat(balance.innerText);
+    const balanceAmount = salary - expenses;
+    balance.innerText = balanceAmount;
+
+}
+function getSavingAmount(SaveField,salary)
+{
+    const UpdateSavingAmount = (SaveField * salary) / 100;
+    const savingAmount = document.getElementById('saving-amount');
+    const previousSavingAmount = parseFloat(savingAmount.innerText);
+    savingAmount.innerText = UpdateSavingAmount;
 }
 
 
@@ -17,5 +32,16 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodCost = getInput('food');
     const rentCost = getInput('rent');
     const clothesCost = getInput('clothes');
-    getTotalExpenses(foodCost, rentCost, clothesCost);
+    const salary = getInput('salary');
+    const total = getTotalExpenses(foodCost, rentCost, clothesCost);
+    getBalance(total, salary);
+
+})
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const SaveField = getInput('save');
+    const salary = getInput('salary');
+   getSavingAmount(SaveField,salary);
+
+
 })
