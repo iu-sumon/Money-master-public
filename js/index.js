f25475d995b50e42defc78e4ee2bbe1140459b58
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-02-17 10:42:48 +0600

    remaining balance added

--- js/index.js
+++ js/index.js
@@ -1,47 +1,73 @@
 function getInput(list) {
     const expensesField = document.getElementById(list + '-field');
     const expensesFieldValue = parseFloat(expensesField.value);
-    expensesField.value = '';
+    //expensesField.value = '';
     return expensesFieldValue;
 
 }
 function getTotalExpenses(food, rent, clothes) {
-    const totalExpenses = document.getElementById('total-expenses');
-    const totalExpensesValue = parseFloat(totalExpenses.innerText);
-    const total = food + rent + clothes + totalExpensesValue;
-    totalExpenses.innerText = total;
-    return total;
-}
-function getBalance(expenses, salary) {
-    const balance = document.getElementById('balance');
-    const previousBalance = parseFloat(balance.innerText);
-    const balanceAmount = salary - expenses;
-    balance.innerText = balanceAmount;
 
-}
-function getSavingAmount(SaveField,salary)
-{
-    const UpdateSavingAmount = (SaveField * salary) / 100;
-    const savingAmount = document.getElementById('saving-amount');
-    const previousSavingAmount = parseFloat(savingAmount.innerText);
-    savingAmount.innerText = UpdateSavingAmount;
+    const previousTotalExpenses = document.getElementById('total-expenses');
+    const previousTotalExpensesValue = parseFloat(previousTotalExpenses.innerText);
+    const updateTotalExpenses = food + rent + clothes + previousTotalExpensesValue;
+    previousTotalExpenses.innerText = updateTotalExpenses;
+    return updateTotalExpenses;
 }
 
+function getBalance(salary, totalExpenses) {
+
+    const previousBalance = document.getElementById('balance');
+    const previousBalanceValue = parseFloat(previousBalance.innerText);
+    const updateBalanceAmount = salary - totalExpenses;
+    previousBalance.innerText = updateBalanceAmount;
+    return updateBalanceAmount;
+}
+
+function getSavingAmount(salaryAmount, saveAmount) {
+    const previousSavingAmount = document.getElementById('saving-amount');
+    const previousSavingAmountValue = parseFloat(previousSavingAmount.innerText);
+    const updateSavingAmount = (salaryAmount * saveAmount) / 100;
+    previousSavingAmount.innerText = updateSavingAmount;
+    return updateSavingAmount;
+}
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const foodCost = getInput('food');
-    const rentCost = getInput('rent');
-    const clothesCost = getInput('clothes');
-    const salary = getInput('salary');
-    const total = getTotalExpenses(foodCost, rentCost, clothesCost);
-    getBalance(total, salary);
+    const currentFoodAmount = getInput('food');
+    const currentRentAmount = getInput('rent');
+    const currentClothesAmount = getInput('clothes');
+    const currentSalaryAmount = getInput('salary');
+    const currentTotalExpenses = getTotalExpenses(currentFoodAmount, currentRentAmount, currentClothesAmount);
+    getBalance(currentSalaryAmount, currentTotalExpenses);
 
 })
-
 document.getElementById('save-btn').addEventListener('click', function () {
-    const SaveField = getInput('save');
-    const salary = getInput('salary');
-   getSavingAmount(SaveField,salary);
-
-
+    const newSavePercentage = getInput('save');
+    const newSalaryAmount = getInput('salary');
+   const newSavingAmount=  getSavingAmount(newSalaryAmount, newSavePercentage);
+   const  currentBalance=document.getElementById('balance').innerText;
+    const previousRemainingBalance = document.getElementById('remaining-balance');
+     const previousRemainingBalanceValue = parseFloat(previousRemainingBalance.innerText);
+     const updateRemainingBalance=(currentBalance-newSavingAmount);
+     previousRemainingBalance.innerText = updateRemainingBalance;
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRemainingBalance(updateBalanceAmount, UpdateSavingAmount) {
+   
+// }
+
+
+
+
+
